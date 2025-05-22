abf929eb8908268c98c8aa9f7170962035bd6ac4
Vladislav Onoprienko <vladsbp80@yandex.ru>
2025-05-22 20:21:24 +0300

    Исправления по CodeStyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     List<Film> getAllFilms();
+
     Film getFilmById(long id);
+
     Film createFilm(Film film);
+
     Film updateFilm(Film film);
+
     void clear();
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     List<User> getAllUsers();
+
     User getUserById(long id);
+
     User createUser(User user);
+
     User updateUser(User user);
+
     void clear();
 }
