98ef11d882d7c8adfaf46c3e509188e8da95395e
Vladislav-Onoprienko <vladsbp80@yandex.ru>
2025-06-29 01:31:10 +0300

    Итоговая реализация ФЗ-12

--- src/main/java/ru/yandex/practicum/filmorate/validator/Constants.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/Constants.java
@@ -6,5 +6,6 @@ public final class Constants {
     public static final Set<Long> VALID_MPA_IDS = Set.of(1L, 2L, 3L, 4L, 5L);
     public static final Set<Long> VALID_GENRE_IDS = Set.of(1L, 2L, 3L, 4L, 5L, 6L);
 
-    private Constants() {}
+    private Constants() {
+    }
 }
